13015f048a4f365b03cec86ffa7790b37137135d
Frank <271733455@qq.com>
2020-12-05 17:45:09 +0800

    添加商品

--- src/js/pages/Goods/GoodsSearch.jsx
+++ src/js/pages/Goods/GoodsSearch.jsx
@@ -41,220 +41,6 @@ const { RangePicker } = DatePicker;
 const { Option } = Select;
 
 @Form.create()
-class GoodsAction extends React.PureComponent {
-  state = {
-    visible: false,
-  };
-
-  static expando = 0;
-
-  showDrawer = () => {
-    this.setState({ visible: true });
-  };
-
-  closeDrawer = () => {
-    this.setState({ visible: false });
-  };
-
-  handleAddSubmit = (e) => {
-    const { refresh, form } = this.props;
-
-    form.validateFields((err, values) => {
-      if (!err) {
-        this.setState({ visible: false });
-        request('/api/insert_goods', {
-          method: 'post',
-          body: { ...values, id: this.props.id, user_name: this.props.user_name, user_id: this.props.user_id, status: 0 },
-        }).then((payload) => {
-          message.success('添加成功！');
-          form.resetFields();
-          refresh();
-        });
-      }
-    });
-  };
-
-  goodsAdd = () => {
-    const { history } = this.props;
-
-    history.push('/goodsadd');
-  };
-
-  render() {
-    const { type, data } = this.props;
-    const { getFieldDecorator } = this.props.form;
-
-    return (
-      <Fragment>
-        {type == 'add' ? (
-          <Button type="primary" style={{ marginBottom: 3 }} icon="plus" onClick={this.showDrawer} onClick={this.goodsAdd}>
-            添加商品
-          </Button>
-        ) : (
-          <a style={{ margin: '0 5px' }} onClick={this.showDrawer}>
-            编辑
-          </a>
-        )}
-        <Drawer
-          width={800}
-          title={type == 'add' ? '新增商品' : '编辑商品'}
-          closable={false}
-          onClose={this.closeDrawer}
-          visible={this.state.visible}
-        >
-          <Form
-            onSubmit={type == 'add' ? this.handleAddSubmit : this.handleEditSubmit}
-            className={`${styles.goodsOperate} fn-clear`}
-          >
-            <section>
-              <FormItem label="商品名称">
-                {getFieldDecorator('name', {
-                  initialValue: data && data.name,
-                  rules: [{ required: true, message: '请输入商品名称' }],
-                })(<Input placeholder="请输入商品名称" />)}
-              </FormItem>
-            </section>
-            <section>
-              <FormItem label="库存量">
-                {getFieldDecorator('inventory_quantity', {
-                  initialValue: data && data.inventory_quantity,
-                  rules: [{ required: true, message: '请输入库存量' }],
-                  pattern: new RegExp(/^[1-9]\d*$/, 'g'),
-                  message: '请输入库存量',
-                  getValueFromEvent: (event) => {
-                    return event.target.value.replace(/\D/g, '');
-                  },
-                })(<Input placeholder="请输入库存量" />)}
-              </FormItem>
-            </section>
-            <section>
-              <FormItem label="销售价格">
-                {getFieldDecorator('seling_price', {
-                  initialValue: data && data.seling_price,
-                  rules: [{ required: true, message: '请输入销售价格' }],
-                  pattern: new RegExp(/^[1-9]\d*$/, 'g'),
-                  message: '请输入销售价格',
-                  getValueFromEvent: (event) => {
-                    return event.target.value.replace(/\D/g, '');
-                  },
-                })(<Input placeholder="请输入销售价格" />)}
-              </FormItem>
-            </section>
-            <section>
-              <FormItem label="库存下线">
-                {getFieldDecorator('min_num', {
-                  initialValue: data && data.min_num,
-                  rules: [{ required: true, message: '请输入库存下线' }],
-                  pattern: new RegExp(/^[1-9]\d*$/, 'g'),
-                  message: '请输入库存下线',
-                  getValueFromEvent: (event) => {
-                    return event.target.value.replace(/\D/g, '');
-                  },
-                })(<Input placeholder="请输入库存下线" />)}
-              </FormItem>
-            </section>
-            <section>
-              <FormItem label="商品单位">
-                {getFieldDecorator('unit', {
-                  initialValue: data && data.unit,
-                  rules: [{ required: true, message: '请输入商品单位' }],
-                })(<Input placeholder="请输入商品单位" />)}
-              </FormItem>
-            </section>
-            <section>
-              <FormItem label="货单编号">
-                {getFieldDecorator('code_id', {
-                  initialValue: data && data.code_id,
-                  pattern: new RegExp(/^[1-9]\d*$/, 'g'),
-                  message: '请输入货单编号',
-                  getValueFromEvent: (event) => {
-                    return event.target.value.replace(/\D/g, '');
-                  },
-                })(<Input placeholder="请输入货单编号" />)}
-              </FormItem>
-            </section>
-            <section>
-              <FormItem label="供应商条码">
-                {getFieldDecorator('s_code', {
-                  initialValue: data && data.s_code,
-                })(<Input placeholder="请输入供应商条码" />)}
-              </FormItem>
-            </section>
-            <section>
-              <FormItem label="自定义条码">
-                {getFieldDecorator('u_code', {
-                  initialValue: data && data.u_code,
-                })(<Input placeholder="请输入自定义条码" />)}
-              </FormItem>
-            </section>
-            <section>
-              <FormItem label="商品链接">
-                {getFieldDecorator('s_link', {
-                  initialValue: data && data.s_link,
-                })(<Input placeholder="请输入商品链接" />)}
-              </FormItem>
-            </section>
-            <section>
-              <FormItem label="商品图片">
-                {getFieldDecorator('s_photo', {
-                  initialValue: data && data.s_photo,
-                })(<Input placeholder="请输入图片地址" />)}
-              </FormItem>
-            </section>
-            <section>
-              <FormItem label="商品分类">
-                {getFieldDecorator('type_id', {
-                  initialValue: data && data.type_id,
-                })(<Input placeholder="请输入商品分类" />)}
-              </FormItem>
-            </section>
-            <section>
-              <FormItem label="商品品类">
-                {getFieldDecorator('unit_pinlei', {
-                  initialValue: data && data.unit_pinlei,
-                })(<Input placeholder="请输入商品品类" />)}
-              </FormItem>
-            </section>
-            <section>
-              <FormItem label="阈值提醒">
-                {getFieldDecorator('threshold_remind', {
-                  initialValue: data && data.threshold_remind,
-                })(<Input placeholder="请输入阈值" />)}
-              </FormItem>
-            </section>
-            <div
-              style={{
-                position: 'absolute',
-                bottom: 0,
-                width: '100%',
-                borderTop: '1px solid #e8e8e8',
-                padding: '10px 16px',
-                textAlign: 'right',
-                left: 0,
-                background: '#fff',
-                borderRadius: '0 0 4px 4px',
-              }}
-            >
-              <Button
-                style={{
-                  marginRight: 8,
-                }}
-                onClick={this.closeDrawer}
-              >
-                取消
-              </Button>
-              <Button htmlType="submit" type="primary">
-                确定
-              </Button>
-            </div>
-          </Form>
-        </Drawer>
-      </Fragment>
-    );
-  }
-}
-
-@Form.create()
 class ChangeGroup extends React.Component {
   state = {
     visible: false,
@@ -515,6 +301,11 @@ class GoodsTable extends React.Component {
     this.props.table.search();
   };
 
+  handleAdd = () => {
+    const { history } = this.props;
+    history.push('/goodsadd');
+  };
+
   render() {
     const { table, supply, user_name, user_id, ...restProps } = this.props;
     const { goodsSort } = this.state;
@@ -523,13 +314,9 @@ class GoodsTable extends React.Component {
       <Fragment>
         <h2 className="title">
           <span>商品</span>
-          <GoodsAction
-            id={this.props.id}
-            refresh={this.refresh}
-            user_name={user_name}
-            history={this.props.history}
-            user_id={user_id}
-          ></GoodsAction>
+          <Button type="primary" onClick={this.handleAdd}>
+            添加商品
+          </Button>
         </h2>
         <FormSearch onSearch={this.handleSearch}>
           {({ form }) => {
